a5bdad5fb3e4cbc3287d31b755b013c1cccc1da9
Vasiliy55578 <63239498+Vasiliy55578@users.noreply.github.com>
2020-11-12 15:44:49 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ru">
+	<head>
+		<title>Главная</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+<br><br>
+<form action="php.php" method="POST">
+<h2>Введите Ключ: <input name="name" placeholder="Введите Ключ:" ></input> <button>Поиск</button></h2>
+</form>
+	<br><br>
+	<div class="page">	
+		<div class="header">
+			<div class="inner">		
+		
+		<div class="logo" > 
+		<? 
+		include_once 'php.php';
+for( $k = 0; $k < count($znach); $k++ ) {
+	echo "<div class='logo1' >" .$namkey ."</div> " .$znach[$k]."<hr>";
+}
+	?>  
+</div>
+			</div>										
+		</div>
+	</div>	
+
+	</body>
+</html>
---
+++ php.php
@@ -0,0 +1,57 @@
+<?  
+if(isset($_POST['name'])) {$namkey = $_POST['name']; }	//введенное значение
+
+		$section = file_get_contents('2_1.txt');	//получение данных с файла и занесение в переменную
+
+for( $i = 0; $i < strlen($section); $i++ ) {
+
+		if( $section[$i] == "=") {	
+			$name[$i] = ",";
+		}
+
+		if( $section[$i] !== "=") {
+			if( $section[$i] !== "[") {	
+				if(trim($section[$i])){ // убрать пробелы
+				$name[$i] =  $section[$i];
+				}
+			}
+		}
+
+	if( $section[$i] == "[") {
+		do {
+		$i++;
+		if( $section[$i] == "]" ){	// если элемент из массива равен ]
+		$section[$i] ='/';	// разделяем названия слешем
+		$Str[$i] = $section[$i];
+		break;	// если наткнулся на ] ломаем 
+		}else{
+			$Str[$i] = $section[$i];	// получаем строку с названиями в [ ]
+		}
+
+		} while ($section[$i] !== "]");
+	}
+}
+
+$Str = implode($Str); // изменение массива символов в строку
+$array = explode('/', $Str);	// разбиение на массив по слешу, массив из того что в [ ]
+
+$name = implode($name); 
+$name = explode(',', $name); //разбиение на массив по запятой, получение массива имен
+
+$l1 = 0;
+$v = 0;
+// создание массива в паре ключ и значение - logo_part_1 => Mastering UI
+for( $k = 0; $k < count($name); $k++ ) {
+		if( $name[$k] == $namkey){	
+			$znach[$v] = $array[$k];
+			$v++;
+		}
+		elseif(( $name[$k] != $namkey)AND($l1 == 0)){	
+			$l1 = 1;
+			$znach[$k] ="undef";
+		}
+		
+	}
+
+	include_once "index.php"; 
+?>  
